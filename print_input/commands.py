d0c5e7c634dacec65e7716bd7ff1b79d2aa705d3
alesia <tmshwsalsh@gmail.com>
2025-06-13 00:28:30 +0300

    feat: added commands print, input

---
+++ print_input/commands.py
@@ -0,0 +1,11 @@
+# syntax
+a=5
+print(a)
+
+# task. calculate expressions (sum, subtraction, division, multiplication)
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("sum:", a + b)
+print("subtraction", a - b)
+print("division", a / b)
+print("multiplication", a * b)
