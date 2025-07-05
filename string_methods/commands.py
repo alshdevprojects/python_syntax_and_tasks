520d855cc253ead01c8a06f884bedee412640384
alesia <tmshwsalsh@gmail.com>
2025-07-05 14:45:15 +0300

    feat: added string commands

---
+++ string_methods/commands.py
@@ -0,0 +1,105 @@
+# 1. capitalize() - returns a copy of the string with the first letter converted to uppercase and the rest converted
+#               to lowercase.
+
+# 2. casefold() - returns a copy of the string with the case folded. This is similar to converting to lowercase,
+#             but more aggressive.
+
+# 3. center(width[, fillchar]) - centers the specified string, padding it to the right and left of the specified
+#                             length with the specified character.
+
+# 4. count(sub[, start[, end]]) - given a string, returns the number of non-overlapping occurrences of the specified
+#                              substring.
+
+# 5. encode(encoding="utf-8", errors="strict") - encodes a string to bytes/bytestring using the registered codec.
+
+# 6. endswith(suffix[, start[, end]]) - returns True or False indicating whether the string ends with the specified
+#                                    suffix.
+
+# 7. expandtabs(tabsize=8) - returns a copy of the string with tabs replaced with spaces.
+
+# 8. find(sub[, start[, end]]) - searches for a substring in a string. Returns the lowest index at which the start
+#                             of the specified substring is found in the original string.
+
+# 9. format(args, *kwargs) - returns a formatted copy of the string. The method replaces markers enclosed in curly
+#                         braces {} with the values from the passed arguments.
+
+# 10. index(sub[, start[, end]]) - returns the lowest index at which the start of the specified substring is found
+#                                  in the original string.
+
+# 11. isalnum() - returns True or False indicating whether the string contains only numbers and/or letters.
+
+# 12. isalpha() - returns True or False indicating whether the string contains only letters.
+
+# 13. isascii() - returns True or False indicating whether the string contains only ASCII characters.
+
+# 14. isdigit() - returns True or False indicating whether the string contains only digits.
+
+# 15. isidentifier() - returns True or False indicating whether the string is an identifier.
+
+# 16. islower() - returns True or False indicating whether the string contains only lowercase characters.
+
+# 17. isnumeric() - returns True or False indicating whether the string contains only numbers.
+
+# 18. isprintable() - returns True or False indicating whether all characters in the string are printable.
+#                     Returns True if the string is empty or if all of its characters are printable.
+
+# 19. isspace() - returns True or False indicating whether the string contains only whitespace characters.
+
+# 20. istitle() - returns True or False indicating whether each "word" of the string begins with a capital letter.
+
+# 21. isupper() - returns True or False indicating whether the string contains only uppercase characters.
+
+# 22. join(iterable) - returns a string constructed from the elements of the specified iterable.
+
+# 23. ljust(width[, fillchar]) - left-aligns the specified string, padding it with the specified character
+#                                to the right to the specified length.
+
+# 24. lower() - returns a copy of the original string with the characters converted to lowercase.
+
+# 25. lstrip([chars]) - returns a copy of the specified string with the specified characters removed
+#                       from the beginning (left l - left).
+
+# 26. maketrans(x[, y[, z]]) - returns a translation table that can be used in the translate method.
+
+# 27. partition(sep) - splits the string into three components (start, separator, end) and returns as a tuple.
+
+# 28. replace(old, new[, count]) - returns a copy of the string with all occurrences of the specified string
+#                                  replaced with the specified value.
+
+# 29. rfind(sub[, start[, end]]) - returns the indices of the last occurrence of the searched substring.
+#                                  If the substring is not found, the method returns the value −1.
+
+# 30. rindex(sub[, start[, end]]) - returns the highest index at which the end of the specified substring
+#                                   is found in the original.
+
+# 31. rjust(width[, fillchar]) - positions the specified string to the right, padding it on the left with
+#                                the specified character to the specified length.
+
+# 32. rpartition(sep) - splits the string into three components (start, delimiter, end) and returns them as a tuple.
+#                       The direction of the split is from right to left.
+
+# 33. rsplit(sep=None, maxsplit=-1) - splits the string into parts using the delimiter, and returns these parts
+#                                     as a list. The direction of the split is from right to left.
+
+# 34. rstrip([chars]) - returns a copy of the specified string with the specified characters removed from the end.
+
+# 35. split(sep=None, maxsplit=-1) - splits a string into pieces using a separator, and returns the pieces as a list.
+
+# 36. splitlines([keepends]) - splits a string into multiple lines, returning them as a list.
+
+# 37. startswith(prefix[, start[, end]]) - returns True or False indicating whether the string starts
+#                                          with the specified prefix.
+
+# 38. strip([chars]) - returns a copy of the specified string with the specified characters removed from both ends.
+
+# 39. swapcase() - returns a copy of the string with each letter case reversed.
+
+# 40. title() - returns a copy of the string with each new word starting with a capital letter
+#               and continuing with lowercase.
+
+# 41. translate(table) - returns a string converted using a translation table, which in turn can be
+#                        obtained using maketrans.
+
+# 42. upper() - returns a copy of the original string with characters converted to uppercase.
+
+# 43. zfill(width) - pads the specified string with zeros on the left to the specified minimum length.
