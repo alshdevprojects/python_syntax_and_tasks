92593bba290ba7acb3d2929acd0132a86e98846d
alesia <tmshwsalsh@gmail.com>
2025-07-06 22:02:40 +0300

    fix task for string methods

--- string_methods/tasks.py
+++ string_methods/tasks.py
@@ -9,6 +9,7 @@ b = input('Enter your text: ')
 print('Your formatted text: ', b.casefold())
 
 # 3. center(width[, fillchar])
-# task.
+# task.From the text entered by the user, make a resulting string of 10 characters. If the text is less than
+#      10 characters, then add the characters 1 on both sides
 c = input('Enter your text: ')
 print(c.center(10, '1'))
